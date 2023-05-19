3eb7c26b3ef988204802b4373815f7d357887064
SergiusMergius <134052930+SergiusMergius@users.noreply.github.com>
2023-05-19 15:26:32 +0600

    Add files via upload

---
+++ Read.php
@@ -0,0 +1,57 @@
+<?php
+
+// необходимые HTTP-заголовки
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// подключение базы данных и файл, содержащий объекты
+include_once "../config/database.php";
+include_once "../objects/item.php";
+
+// получаем соединение с базой данных
+$database = new Database();
+$db = $database->getConnection();
+
+// инициализируем объект
+$item = new Item($db);
+
+// запрашиваем товары
+$stmt = $item->read();
+$num = $stmt->rowCount();
+
+// проверка, найдено ли больше 0 записей
+if ($num > 0) {
+    // массив товаров
+    $item_arr = array();
+    $item_arr["records"] = array();
+
+    // получаем содержимое нашей таблицы
+    // fetch() быстрее, чем fetchAll()
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // извлекаем строку
+        extract($row);
+        $item_item = array(
+            "id" => $id,
+            "name" => $name,
+            "phone" => $phone,
+            "key" => $key,
+            "created_at" => $created_at,
+            "updated_at" => $updated_at
+        );
+        array_push($item_arr["records"], $item_item);
+    }
+
+    // устанавливаем код ответа - 200 OK
+    http_response_code(200);
+
+    // выводим данные о товаре в формате JSON
+    echo json_encode($item_arr);
+}
+
+else {
+    // установим код ответа - 404 Не найдено
+    http_response_code(404);
+
+    // сообщаем пользователю, что товары не найдены
+    echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
+}
